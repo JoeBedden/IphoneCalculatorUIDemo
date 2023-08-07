9342c6e66bcbdea8bbd83fba8faf9bd67a41b36a
LeiZiKang <lzkhaoshua@icloud.com>
2023-08-08 06:50:54 +0800

    Initial Commit

---
+++ IphoneCalculatorUIDemo/IphoneCalculatorUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneCalculatorUIDemoApp.swift
+//  IphoneCalculatorUIDemo
+//
+//  Created by 雷子康 on 2023/8/8.
+//
+
+import SwiftUI
+
+@main
+struct IphoneCalculatorUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
